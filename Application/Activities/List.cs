ebbc7c2ef22b0665e099458ccfca003bed583493
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-11-22 20:26:05 -0800

    Added styled navbar

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -22,7 +22,6 @@ namespace Application.Activities
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                
                 return await _context.Activities.ToListAsync();
             }
         }
