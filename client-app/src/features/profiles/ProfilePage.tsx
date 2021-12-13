35f2f1436ca6c4ddd7011bdf888f8a8b29e6f878
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-12-12 20:50:59 -0800

    added Profile header

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,8 +1,14 @@
 import { observable } from 'mobx'
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import ProfileHeader from './ProfileHeader'
 
 export default function ProfilePage() {
     return (
-        <h1>Profile</h1>
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader />
+            </Grid.Column>
+        </Grid>
     )
 }
